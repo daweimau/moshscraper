95f7a07488f291f2f0a5a49a3ade0039778ac8bc
David Marshall <marshalldau22@gmail.com>
2021-06-13 14:54:42 +1000

    in-progress work towards staggering downloads a bit

--- index.js
+++ index.js
@@ -8,17 +8,29 @@ var fs = require("fs");
 function main() {
     const rawdata = fs.readFileSync("scraped_links.json");
     const sections = JSON.parse(rawdata);
-    sections.forEach((s) => {
-        const sPath = `course_content/${s.sectionTitle}`;
+
+    sections.forEach(async (section) => {
+        const sPath = `course_content/${section.sectionTitle}`;
         fs.mkdirSync(sPath, { recursive: true });
 
-        s.content.forEach((lec) => {
-            if (lec.downloadLink) {
-                const dest = `${sPath}/${lec.title}.mp4`;
-                if (!fs.existsSync(dest)) download(lec.downloadLink, dest);
-                else console.log(`${dest} already exists. Skipping...`);
-            }
-        });
+        // The promise implementation was an attempt to restrict downloading
+        // just one section at a time. Currently it's still downloading everything at once.
+        await Promise.all(
+            section.content.map((lec) => {
+                return new Promise(async (resolve) => {
+                    if (lec.downloadLink) {
+                        const dest = `${sPath}/${lec.title}.mp4`;
+                        if (!fs.existsSync(dest)) {
+                            await download(lec.downloadLink, dest);
+                            resolve();
+                        } else {
+                            console.log(`${dest} already exists. Skipping...`);
+                            resolve();
+                        }
+                    }
+                });
+            })
+        );
     });
 }
 
@@ -26,23 +38,33 @@ function clearIfExists(path) {
     if (fs.existsSync(path)) fs.unlinkSync(path);
 }
 
-// This is asynchronous, but doesn't use promises
-function download(url, dest, cb) {
+// Async
+function download(url, dest, callback) {
     const placeholderPath = `${dest}-incomplete`;
 
-    // Remove any previous incomplete downloads
-    clearIfExists(placeholderPath);
+    return new Promise((resolve) => {
+        // Remove any previous incomplete downloads
+        clearIfExists(placeholderPath);
 
-    // Create a new placeholder file
-    var file = fs.createWriteStream(placeholderPath);
-    http.get(url, function (response) {
-        response.pipe(file);
-        file.on("finish", function () {
-            file.close(cb);
-            // The download has complete: remove the placeholder flag
-            fs.renameSync(placeholderPath, dest);
-            console.log(`Completed download for: ${dest}`);
-        });
+        // Create a new placeholder file
+        var file = fs.createWriteStream(placeholderPath);
+        try {
+            http.get(url, function (response) {
+                response.pipe(file);
+                file.on("finish", function () {
+                    file.close(callback);
+                    // The download has complete: remove the placeholder flag
+                    fs.renameSync(placeholderPath, dest);
+                    console.log(`Completed download for: ${dest}`);
+                    resolve();
+                });
+            });
+        } catch (err) {
+            file.close(callback);
+            console.log(`This download failed: ${dest}`);
+            console.log(err);
+            resolve();
+        }
     });
 }
 
